d71a35d651c074a2bf1ac68e695f43e17e9609a4
AlexCsanyi <alex.csanyi@gmail.com>
2019-03-21 08:05:17 +0000

    basic js and css set up

--- myMarquee/assets/includes/marquee.js
+++ myMarquee/assets/includes/marquee.js
@@ -1,2 +1,28 @@
 /* JavaScript Document */
 
+var marqueeVars = {
+  screenSize: "",
+  width: 0,
+  mobileSize: 600,
+  autoPlay: true,
+  currentPanel: 1,
+  totalPanels: 0,
+  timePassed: 0,
+  timeToChange: 60,
+  duration: 1250,
+  inTransition: false,
+  panelContent: Array
+};
+
+var debugTimer = setInterval(setDebugger, 100);
+function setDebugger() {
+  $(".var1").html("screenSize = " + marqueeVars.screenSize);
+  $(".var2").html("width = " + marqueeVars.width);
+  $(".var3").html("mobileSize = " + marqueeVars.mobileSize);
+  $(".var4").html("autoPlay = " + marqueeVars.autoPlay);
+  $(".var5").html("currentPanel = " + marqueeVars.currentPanel);
+  $(".var6").html("totalPanels = " + marqueeVars.totalPanels);
+  $(".var7").html("timePassed = " + marqueeVars.timePassed);
+  $(".var8").html("timeToChange = " + marqueeVars.timeToChange);
+  $(".var9").html("inTransition = " + marqueeVars.inTransition);
+}
